bac33c700075e8f9b7702f7a4a229f43108f8a15
Douglas Lourenço Siqueira <douglas.lourencos@gmail.com>
2022-12-17 10:00:01 -0300

    implemented border radius previewer

--- README.md
+++ README.md
@@ -1,27 +1,17 @@
-# AngularBorderRadiusPreviewer
+# Border-radius Previewer
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.3.
+Idea from [here](https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Border-Radius-Previewer.md)
 
-## Development server
+**Demo**: [here](https://d-siqueira.github.io/angular-border-radius-previewer/)
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+The border-radius property can have multiple values changed. Preview how the shape looks while changing these values.
 
-## Code scaffolding
+## User Stories
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+-   [x] User can see a box which has a `border-radius` property applied to it
+-   [x] User can change the 4 `border-radius` values that are applied to the box (top-left, top-right, bottom-left, bottom-right)
+-   [x] User can copy the resulting CSS to the clipboard
 
-## Build
+## Bonus features
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+-   [x] User can change all 8 possible values of the border-radius in order to create a complex shape
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,143 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { faLink, faUnlink, faLock, faLockOpen } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-border-radius-previewer';
+export class AppComponent implements OnInit {
+  @ViewChild('cssBorderRadius') cssBorderRadius!: ElementRef<HTMLTextAreaElement>;
+
+  icon = { faLock, faLockOpen };
+  formGroup: FormGroup;
+  topLeftStyle = '';
+  topRightStyle = '';
+  bottomLeftStyle = '';
+  bottomRightStyle = '';
+
+  constructor(fb: FormBuilder) {
+    this.formGroup = fb.group({
+      topLeftHorizontal: 0,
+      topLeftVertical: 0,
+      sameValueTopLeft: true,
+
+      topRightHorizontal: 0,
+      topRightVertical: 0,
+      sameValueTopRight: true,
+
+      bottomLeftHorizontal: 0,
+      bottomLeftVertical: 0,
+      sameValueBottomLeft: true,
+
+      bottomRightHorizontal: 0,
+      bottomRightVertical: 0,
+      sameValueBottomRight: true
+    });
+  }
+
+  ngOnInit(): void {
+    this.formGroup.get('topLeftHorizontal')?.valueChanges.subscribe({
+      next: value => {
+        const sameValue: boolean = this.formGroup.get('sameValueTopLeft')?.value;
+        if(sameValue){
+          this.formGroup.get('topLeftVertical')?.setValue(value, { emitEvent: false });
+        }
+        this.topLeftStyle = this.topLeft();
+      }
+    });
+    this.formGroup.get('topLeftVertical')?.valueChanges.subscribe({
+      next: value => {
+        const sameValue: boolean = this.formGroup.get('sameValueTopLeft')?.value;
+        if(sameValue){
+          this.formGroup.get('topLeftHorizontal')?.setValue(value, { emitEvent: false });
+        }
+        this.topLeftStyle = this.topLeft();
+      }
+    });
+
+    this.formGroup.get('topRightHorizontal')?.valueChanges.subscribe({
+      next: value => {
+        const sameValue: boolean = this.formGroup.get('sameValueTopRight')?.value;
+        if(sameValue){
+          this.formGroup.get('topRightVertical')?.setValue(value, { emitEvent: false });
+        }
+        this.topRightStyle = this.topRight();
+      }
+    });
+    this.formGroup.get('topRightVertical')?.valueChanges.subscribe({
+      next: value => {
+        const sameValue: boolean = this.formGroup.get('sameValueTopRight')?.value;
+        if(sameValue){
+          this.formGroup.get('topRightHorizontal')?.setValue(value, { emitEvent: false });
+        }
+        this.topRightStyle = this.topRight();
+      }
+    });
+
+    this.formGroup.get('bottomLeftHorizontal')?.valueChanges.subscribe({
+      next: value => {
+        const sameValue: boolean = this.formGroup.get('sameValueBottomLeft')?.value;
+        if(sameValue){
+          this.formGroup.get('bottomLeftVertical')?.setValue(value, { emitEvent: false });
+        }
+        this.bottomLeftStyle = this.bottomLeft();
+      }
+    });
+    this.formGroup.get('bottomLeftVertical')?.valueChanges.subscribe({
+      next: value => {
+        const sameValue: boolean = this.formGroup.get('sameValueBottomLeft')?.value;
+        if(sameValue){
+          this.formGroup.get('bottomLeftHorizontal')?.setValue(value, { emitEvent: false });
+        }
+        this.bottomLeftStyle = this.bottomLeft();
+      }
+    });
+
+    this.formGroup.get('bottomRightHorizontal')?.valueChanges.subscribe({
+      next: value => {
+        const sameValue: boolean = this.formGroup.get('sameValueBottomRight')?.value;
+        if(sameValue){
+          this.formGroup.get('bottomRightVertical')?.setValue(value, { emitEvent: false });
+        }
+        this.bottomRightStyle = this.bottomRight();
+      }
+    });
+    this.formGroup.get('bottomRightVertical')?.valueChanges.subscribe({
+      next: value => {
+        const sameValue: boolean = this.formGroup.get('sameValueBottomRight')?.value;
+        if(sameValue){
+          this.formGroup.get('bottomRightHorizontal')?.setValue(value, { emitEvent: false });
+        }
+        this.bottomRightStyle = this.bottomRight();
+      }
+    });
+  }
+
+  copyCss(): void {
+    this.cssBorderRadius.nativeElement.select();
+    const css = this.cssBorderRadius.nativeElement.value;
+    navigator.clipboard.writeText(css);
+  }
+
+  toggleValueCheckbox(formControl: AbstractControl | null) {
+    formControl?.setValue(!formControl.value);
+  }
+
+  private topLeft(): string {
+    return `${this.formGroup.get('topLeftVertical')?.value ?? 0}% ${this.formGroup.get('topLeftHorizontal')?.value ?? 0}%`;
+  }
+
+  private topRight(): string {
+    return `${this.formGroup.get('topRightVertical')?.value ?? 0}% ${this.formGroup.get('topRightHorizontal')?.value ?? 0}%`;
+  }
+
+  private bottomLeft(): string {
+    return `${this.formGroup.get('bottomLeftVertical')?.value ?? 0}% ${this.formGroup.get('bottomLeftHorizontal')?.value ?? 0}%`;
+  }
+
+  private bottomRight(): string {
+    return `${this.formGroup.get('bottomRightVertical')?.value ?? 0}% ${this.formGroup.get('bottomRightHorizontal')?.value ?? 0}%`;
+  }
 }
